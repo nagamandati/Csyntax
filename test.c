cad03558b75fa50518c9fd8997e676a065f64528
nagamandati <106611541+nagamandati@users.noreply.github.com>
2022-05-31 11:39:59 -0400

    Add files via upload

---
+++ test.c
@@ -0,0 +1,34 @@
+
+
+
+
+
+
+
+
+
+
+#include <iostream>
+using namespace std;
+  int index = 0;
+int main() 
+{
+    cout << "Hello, World!";
+	int a, b, c;
+	cout << "Enter two integers to add\n";
+	cin >> a >> b;
+	c = a + b; 
+	int i = 0;
+	for (i = 1; i <= a ++i) {
+        c *= i;   // factorial = factorial * i;
+    }
+
+	if ( c > 100
+    {
+        cout << "Condition satisfied";
+    } else {
+		cout << "condition failed"
+	}
+
+    return 0;
+}
